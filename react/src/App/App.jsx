729beea803c972b31837e4c4912372fc71e38f1c
denis1222223 <denis1222223@mail.ru>
2017-01-26 16:30:24 +0300

    add spinner custom element

---
+++ react/src/App/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import Hello from '../Hello';
+import '../customElements/x-spinner';
+
+import './app.less';
+
+export default class App extends React.Component {
+    render () {
+        return (
+            <div>
+                <div className="block block1">
+                    <Hello name="World" />
+                    <Hello name="World" />
+                    <Hello name="World" />
+                    <Hello name="World" />
+                    <Hello name="World" />
+
+                    <x-spinner></x-spinner>
+                </div>
+
+                <div className="block block2">
+                    <Hello name="friend" />
+                    <x-spinner></x-spinner>
+                </div>
+
+                <div className="block block3">
+                    <Hello name="Denis" />
+                    <x-spinner></x-spinner>
+                </div>
+            </div>
+        );
+    }
+}
